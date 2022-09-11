12c3d2533f3c0903afdfc5ff4bf324b706992e52
Biatus-Maina <mainabiatus@gmail.com>
2022-09-11 03:27:37 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,11 +4,15 @@
 
 /**
 
- * main - Entry point
+ * main - prints the string in the function
 
  *
 
- * Return: Always 0 (Success)
+ * Description: using the main function
+
+ *this program prints "Programming is like building a multilingual puzzle"
+
+ *return (0);
 
  */
 
