630cdd84f92c15342a7d44e02162682c83ffe6f1
Biatus-Maina <mainabiatus@gmail.com>
2022-09-11 04:56:45 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -5,10 +5,10 @@
 *
 *Description: using the main function
 *this program prints "Programming is like building a multilingual puzzle"
-*return: 0
+*return (0);
 */
 int main(void)
 {
-	printf("with proper grammer, but the outcome is a piece of art,\n);
+	printf("with proper grammer, but the outcome is a piece of art,\n");
 	return (0);
 }
