30afda00c2f980983026bb85c5454a0ee6f57032
Biatus-Maina <mainabiatus@gmail.com>
2022-09-11 05:10:00 -0700

    5-printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 
 /**
-*main - prints the string in the put function
+* main - prints the string in the put function
 *
-*Description: using the main function
-*this program prints "Programming is like building a multilingual puzzle"
-*return (0);
+* Description: using the main function
+* this program prints "Programming is like building a multilingual puzzle"
+* return (0);
 */
 int main(void)
 {
