cb2d12f5bc36bd1f123f3d4471266962177406b0
Biatus-Maina <mainabiatus@gmail.com>
2022-09-11 05:23:08 -0700

    5-printf.c:10: warning: No description found for return value of main

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -4,9 +4,10 @@
  * main - prints the string in the put function
  *
  * Description: using the main function
- * this program prints "Programming is like building a multilingual puzzle"
- * return (0);
+ * 
+ * return: Always 0 (Succes)
  */
+
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
