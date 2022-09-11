d1ea87ec4fa51ffa5ccde7a2cd13e61901d0712a
Biatus-Maina <mainabiatus@gmail.com>
2022-09-11 05:14:32 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
 /**
-* main - prints the string in the put function
-*
-* Description: using the main function
-* this program prints "Programming is like building a multilingual puzzle"
-* return (0);
-*/
+ * main - prints the string in the put function
+ *
+ * Description: using the main function
+ * this program prints "Programming is like building a multilingual puzzle"
+ * return (0);
+ */
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
