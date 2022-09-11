a8b5e432bbcea4e7293c57d7c55d72a7b3df220a
Biatus-Maina <mainabiatus@gmail.com>
2022-09-11 04:38:54 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
- /**
- *main function - prints the string in the function
- *
- *Description: using the main function
- *this program prints "programming is like building a multilingual puzzle"
- *return (0);
- */
+/**
+*main function - prints the string in the function
+*
+*Description: using the main function
+*this program prints "Programming is like building a multilingual puzzle"
+*return (0);
+*/
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
