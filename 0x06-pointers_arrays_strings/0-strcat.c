cbad2b828dcd221e9a6ab46113a21923bd6517f0
Biatus-Maina <mainabiatus@gmail.com>
2022-09-22 13:54:47 -0700

    Pointers and arrays

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _strcat - function commute srtings
+ * *_strcat - function commute srtings
  * @dest: param pointer to a char
  * @src: param pointer to a char
  * Return: return value of dest
@@ -26,5 +26,6 @@ char *_strcat(char *dest, char *src)
 		i++;
 	}
 	dest[i] = '\0';
+
 	return (dest);
 }
