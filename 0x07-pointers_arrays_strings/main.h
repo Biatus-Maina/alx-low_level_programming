acda2f57d7cafae0118b467ce9bfa1131a0551e1
Biatus-Maina <mainabiatus@gmail.com>
2022-09-27 12:50:54 -0700

    int _putchar(char c);
    char *_memset(char *s, char b, unsigned int n);
    
    char *_memcpy(char *dest, char *src, unsigned int n);
    
    char *_strchr(char *s, char c);
    
    unsigned int _strspn(char *s, char *accept);
    
    char *_strpbrk(char *s, char *accept);
    
    char *_strstr(char *haystack, char *needle);
    
    void print_chessboard(char (*a)[8]);
    
    void set_string(char **s, char *to);
    
    void print_diagsums(int *a, int size);

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,5 +1,5 @@
 #ifndef MAIN_H  
-#define MAIN_H 
+#define MAIN_H
 
 int _putchar(char c);
 char *_memset(char *s, char b, unsigned int n);
@@ -10,6 +10,6 @@ char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
 void print_chessboard(char (*a)[8]);
 void set_string(char **s, char *to);
-void set_string(char **s, char *to);
+void print_diagsums(int *a, int size);
 
 #endif
